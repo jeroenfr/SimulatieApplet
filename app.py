cf521fce8d7981eb689f5c178b62f16f24d4ac80
jeroenfr <39625311+jeroenfr@users.noreply.github.com>
2023-12-08 22:05:20 +0100

    init repo

---
+++ app.py
@@ -0,0 +1,64 @@
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+from shiny import App, reactive, render, ui
+import shinyswatch
+
+app_ui = ui.page_fluid(
+    # theme
+    shinyswatch.theme_picker_ui(),
+    
+    
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.input_numeric("n", "Steekproefgrootte", 40),
+            ui.input_slider("p_observed", "Geobserveerde steekproefproportie", value=0.5, min=0, max=1),
+            ui.input_slider("p_0", "Nulhypothese : p_0 = ", value=0.5, min=0, max=1),
+        ),
+        ui.panel_main(
+            ui.output_plot("histogram"),
+            ui.output_text("txt1"),
+            ui.output_table("data")
+        ),
+    ),
+)
+
+
+def server(input, output, session):
+    shinyswatch.theme_picker_server()
+    @reactive.Calc
+    def drempelwaarde():
+        return input.n()*input.p_observed()
+
+    @output
+    @render.text
+    def txt1():
+        return f'Drempelwaarde is "{round(drempelwaarde())}"'
+        
+    #dataset = pd.DataFrame({'waarden': pd.Series(dtype = 'int'), 'vlag':pd.Series(dtype = 'int')})
+    #dataset = pd.DataFrame(columns = ['waarden', 'vlag'])
+        
+    @output
+    @render.table
+    def data():
+        drempel = input.n()*input.p_observed()
+        print("Drempel is: " + str(drempel))
+        x = np.random.binomial(input.n(), input.p_0(), 1000)
+        y = np.where(x>=drempel, 1, 0)
+        df = pd.DataFrame({'waarden': x, 'vlag':y})
+        return df
+
+    @output
+    @render.plot(alt="A histogram")
+    def histogram():
+        drempel = input.n()*input.p_observed()
+        x = np.random.binomial(input.n(), input.p_0(), 1000)
+        y = np.where(x>=drempel, "Groter of gelijk aan steekproef", "Kleiner dan steekproef")
+        df = pd.DataFrame({'waarden': x, 'vlag': y})
+        sns.histplot(data = df , x = 'waarden', hue='vlag', binwidth=1, alpha=0.5, palette=['skyblue', 'salmon'])
+        
+
+
+    
+app = App(app_ui, server, debug=True)
