021fbf1c7d36c257dd336ab8d9195d53cd16a659
jeroenfr <39625311+jeroenfr@users.noreply.github.com>
2023-12-11 16:25:02 +0100

    Input control for number of simulations

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app_ui = ui.page_fluid(
             ui.input_numeric("n", "Steekproefgrootte", 40),
             ui.input_slider("p_observed", "Geobserveerde steekproefproportie", value=0.5, min=0, max=1),
             ui.input_slider("p_0", "Nulhypothese : p_0 = ", value=0.5, min=0, max=1),
+            ui.input_numeric("n_sim", "Aantal simulaties onder nulhypothese", 1000),
         ),
         ui.panel_main(
             ui.output_plot("histogram"),
@@ -44,7 +45,7 @@ def server(input, output, session):
     def data():
         drempel = input.n()*input.p_observed()
         print("Drempel is: " + str(drempel))
-        x = np.random.binomial(input.n(), input.p_0(), 1000)
+        x = np.random.binomial(input.n(), input.p_0(), input.n_sim())
         y = np.where(x>=drempel, 1, 0)
         df = pd.DataFrame({'waarden': x, 'vlag':y})
         return df
@@ -53,7 +54,7 @@ def server(input, output, session):
     @render.plot(alt="A histogram")
     def histogram():
         drempel = input.n()*input.p_observed()
-        x = np.random.binomial(input.n(), input.p_0(), 1000)
+        x = np.random.binomial(input.n(), input.p_0(), input.n_sim())
         y = np.where(x>=drempel, "Groter of gelijk aan steekproef", "Kleiner dan steekproef")
         df = pd.DataFrame({'waarden': x, 'vlag': y})
         plot = sns.histplot(data = df , x = 'waarden', hue='vlag', binwidth=1, alpha=0.5, palette=['skyblue', 'salmon'])
